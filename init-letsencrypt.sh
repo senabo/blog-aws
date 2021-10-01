108a4e78d0c28d8a004fd45db0761ff986863853
Nazarii Senkiv <n.senkiv@bvblogic.com>
2021-10-02 00:19:01 +0300

    fix vars

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -7,8 +7,8 @@ fi
 
 domains=(blog.senabo.space www.blog.senabo.space)
 rsa_key_size=4096
-data_path="./data/certbot"
-email="" # Adding a valid address is strongly recommended
+data_path="./webserver/certbot/"
+email="senabo33@gmail.com" # Adding a valid address is strongly recommended
 staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
