a7ea37e3a37d7ce8eaef3ac9e760ece3630f2e02
Nazarii Senkiv <n.senkiv@bvblogic.com>
2021-10-02 00:55:39 +0300

    update

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -7,10 +7,11 @@ fi
 
 domains=(blog.senabo.space www.blog.senabo.space)
 rsa_key_size=4096
+data_path="./webserver/certbot"
 email="" # Adding a valid address is strongly recommended
 staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
-if `docker-compose run --rm --entrypoint "[ -d /var/www/certbot/conf/live/$domains ]" certbot 2>/dev/null`; then
+if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
   if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
     exit
@@ -18,19 +19,19 @@ if `docker-compose run --rm --entrypoint "[ -d /var/www/certbot/conf/live/$domai
 fi
 
 
-if [ ! `docker-compose run --rm --entrypoint "[ -d /var/www/certbot/conf/options-ssl-nginx.conf ]" certbot 2>/dev/null` ] || [ ! `docker-compose run --rm --entrypoint "[ -d /var/www/certbot/conf/ssl-dhparams.pem ]" certbot 2>/dev/null` ]; then
+if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
   echo "### Downloading recommended TLS parameters ..."
-  docker-compose run --rm --entrypoint "mkdir -p /var/www/certbot/conf" certbot
-  docker-compose run --rm --entrypoint "wget https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf -O /var/www/certbot/conf/options-ssl-nginx.conf" certbot
-  docker-compose run --rm --entrypoint "wget https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem -O /var/www/certbot/conf/ssl-dhparams.pem" certbot
+  mkdir -p "$data_path/conf"
+  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
+  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
   echo
 fi
 
 echo "### Creating dummy certificate for $domains ..."
 path="/etc/letsencrypt/live/$domains"
-docker-compose run --rm --entrypoint "mkdir -p $path" certbot
+mkdir -p "$data_path/conf/live/$domains"
 docker-compose run --rm --entrypoint "\
-  openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
+  openssl req -x509 -nodes -newkey rsa:1024 -days 1\
     -keyout '$path/privkey.pem' \
     -out '$path/fullchain.pem' \
     -subj '/CN=localhost'" certbot
